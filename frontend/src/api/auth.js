8a13e2e1abbd7987e868eb08ad274aa789afaca2
Chirag Mukheja <mukhejachirag22@gmail.com>
2025-06-07 02:31:13 +0530

    fixes

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 
-export const register = async (email, password) => {
+export const register = async (username, password) => {
   const res = await axios.post("https://hotel-logging-bot.onrender.com/auth/register", { username, password });
   return res.data;
 };
 
-export const login = async (email, password) => {
+export const login = async (username, password) => {
   const res = await axios.post("https://hotel-logging-bot.onrender.com/auth/login", { username, password });
   return res.data;
 };
